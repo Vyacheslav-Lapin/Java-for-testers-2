aaa58f8e8671b63a95147ca98ca655efd541b33e
Vyacheslav Lapin <vyacheslav@y-tree.com>
2019-11-01 18:04:49 +0500

    StreamDemo - 2

--- src/main/java/demo/streams/StreamDemo.java
+++ src/main/java/demo/streams/StreamDemo.java
@@ -1,6 +1,7 @@
 package demo.streams;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class StreamDemo {
 
@@ -8,8 +9,9 @@ public class StreamDemo {
     Arrays.stream(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             .split("\\s"))
-        .map(String::length)
-        .max(Integer::compareTo)
-        .ifPresent(System.out::println);
+//        .max(Comparator.comparingInt(String::length))
+        .sorted(Comparator.comparingInt(String::length).reversed())
+        .limit(3)
+        .forEachOrdered(System.out::println);
   }
 }
